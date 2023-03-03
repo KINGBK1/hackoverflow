0864a1b9c22874754607821ed6e36edaba095e4f
aimanaisha <aelinhathway@gmail.com>
2023-03-03 22:21:56 +0530

    judges

--- pages/schedule.js
+++ pages/schedule.js
@@ -116,7 +116,15 @@ export default function SchedulePage() {
                                 </div>
                             </div>
                         </div>
-                        <div className='countDOwn_area'>
+                        <div className='flex flex-col items-center md:items-start px-6 lg:px-52 text-center md:text-left my-6'>
+            <Timer/>
+            <div className='flex flex-col md:flex-row justify-between gap-4 mt-4'>
+            <p className='text-2xl font-bold text-white'>Online</p>
+            <p className='text-2xl font-bold text-white'>9th March, 2023</p>
+
+            </div>
+          </div>
+                        {/* <div className='countDOwn_area'>
                             <div className='container'>
                                 <div className='row align-items-center justify-center'>
                                     <div className='col-xl-4 col-md-6 col-lg-6 flex flex-wrap justify-center'>
@@ -136,7 +144,7 @@ export default function SchedulePage() {
                                     </div>
                                 </div>
                             </div>
-                        </div>
+                        </div> */}
                         <div
                             style={{
                                 width: '100%',
