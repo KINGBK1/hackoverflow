aaff3f715940b1389223ddd902c9e0a8b90ba474
sudhidutta7694 <sudhi.dutta7694@gmail.com>
2024-04-05 20:13:34 +0530

    Changed Sponsors.jsx

--- components/sponsors.jsx
+++ components/sponsors.jsx
@@ -2,36 +2,52 @@ import React from "react";
 import Link from "next/link";
 
 export default function Sponsors() {
-    return (
-        <React.Fragment>
-            <div className="container" style={{ marginTop: "40px" }}>
-                <div className="row">
-                    <div className="col-xl-12">
-                        <div className="serction_title_large mb-3 md:mb-4">
-                            <h3>Sponsors</h3>
-                        </div>
-                        <div  class="flex justify-center align-middle text-xl md:text-3xl mb-2 font-bold">To be released soon!!</div>
-                    </div>
-                </div>
+  return (
+    <React.Fragment>
+      <div className="container" style={{ marginTop: "40px" }}>
+        <div className="row">
+          <div className="col-xl-12">
+            <div className="serction_title_large mb-3 md:mb-4">
+              <h3>Sponsors</h3>
             </div>
-            {/* <div
-                className="w-full pt-2 md:pt-6 md:pb-4 flex md:flex-row flex-col items-center md:justify-around"
-                style={{ marginTop: "30px", marginBottom: "20px" }}
-            >
-                <div className="p-3 mb-10">
-                    <Link href="https://devfolio.co/">
-                        <img src="/devfolio.png" className="h-10 md:h-18" />
-                    </Link>
-                </div>
-                <div className="p-1 mb-10">
-                    <Link href="https://polygon.technology/">
-                        <img
-                            src="/Portis_Logo-Colored.png"
-                            className="h-10 md:h-18"
-                        />
-                    </Link>
-                </div>
-                <div className="p-1 mb-10">
+            {/* <div  class="flex justify-center align-middle text-xl md:text-3xl mb-2 font-bold">To be released soon!!</div> */}
+          </div>
+        </div>
+      </div>
+      <div className="flex justify-center items-center">
+        <div
+          className="md:w-4/5 pt-2 md:pt-6 md:pb-4 flex md:flex-row flex-col items-center md:justify-around"
+          style={{ marginTop: "30px", marginBottom: "20px" }}
+        >
+          <div className="p-3 mb-10">
+            <Link href="https://devfolio.co/">
+              <img
+                alt="DEVFOLIO LOGO"
+                src="/Devfolio.png"
+                className="h-10 md:h-24"
+              />
+            </Link>
+          </div>
+          <div className="p-1 mb-10">
+            <Link href="https://polygon.technology/">
+              <img
+                alt="POLYGON LOGO"
+                src="/Polygon.png"
+                className="h-10 md:h-24"
+              />
+            </Link>
+          </div>
+          <div className="p-1 mb-10">
+            <Link href="https://ethindia.co/">
+              <img
+                alt="ETHINDIA LOGO"
+                src="/ETHIndia.png"
+                className="h-10 md:h-24"
+              />
+            </Link>
+          </div>
+
+          {/* <div className="p-1 mb-10">
                     <Link href="https://replit.com/">
                         <img
                             src="/Replit-light-background.png"
@@ -64,8 +80,9 @@ export default function Sponsors() {
                             className="h-10 md:h-14"
                         />
                     </Link>
-                </div>
-            </div> */}
-        </React.Fragment>
-    );
+                </div> */}
+        </div>
+      </div>
+    </React.Fragment>
+  );
 }
